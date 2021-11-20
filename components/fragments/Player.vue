26d04cfcbc1e4d489cf2b68c73f07d71cce4da9c
Emma <emmandersson166@gmail.com>
2021-11-20 19:51:49 +0100

    added player component

---
+++ components/fragments/Player.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="audio-control">
+    <audio class="controls" controls>
+      <source :src="audioSrc" type="audio/mpeg" />
+    </audio>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["audioSrc"],
+};
+</script>
+
+<style lang="scss">
+.audio-control {
+  z-index: 1;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin-bottom: $padding-m;
+  @include flex-center;
+
+  .controls {
+    width: 80%;
+    margin: 0 auto;
+  }
+}
+</style>
