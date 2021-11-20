0d756600155f1a6b66c3bba2a37a93f99dc2ec9d
Emma <emmandersson166@gmail.com>
2021-11-20 19:47:38 +0100

    added site header

---
+++ components/layout/SiteHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="site-canvas-header">LOGO</div>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+};
+</script>
+
+<style lang="scss">
+.site-canvas-header {
+  @include flex-center;
+
+  height: $site-header-height;
+}
+</style>
